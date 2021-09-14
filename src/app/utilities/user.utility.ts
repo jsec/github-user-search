fb799ae7bee3575a18f4255e36a1cb02253c9d83
Jarrod Seccombe <jsec@hey.com>
2021-09-13 21:27:51 -0400

    refactor helper utilities into their proper location

---
+++ src/app/utilities/user.utility.ts
@@ -0,0 +1,21 @@
+import {
+  IGithubUserObject
+} from '../interfaces/github-user-object.interface';
+import {
+  IUser
+} from '../interfaces/user.interface';
+
+export function getUserFromGithubUserObject(userObj: IGithubUserObject) {
+  return <IUser>{
+    username: userObj.login,
+    name: userObj.name,
+    starCount: 0,
+    followerCount: userObj.followers,
+    avatarUrl: userObj.avatar_url,
+    bio: userObj.bio,
+    company: userObj.company,
+    location: userObj.location,
+    repoCount: userObj.public_repos,
+    followingCount: userObj.following
+  }
+}
